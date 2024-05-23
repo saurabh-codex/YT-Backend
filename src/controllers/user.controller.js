8d794b6a506176ac7b1ffe66e23260415df2b3aa
saurabh soni <130442880+saurabh-codex@users.noreply.github.com>
2024-05-24 00:29:11 +0530

    all setup configured

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
-import { response } from "express";
-import { asyncHandler } from "../utils/asyncHandler";
+
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async(req, res) =>{
     res.status(200).json({
--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 // require('dotenv').config({path: './env'})
 
 import dotenv from "dotenv"
+import { app } from "./app.js";
 
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 
 connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`server is running at port : $
-        {process.env.PORT}`);
+        console.log(`server is running at port:
+        ${process.env.PORT}`);
     })
 })
 .catch((err) => {
-    console.log("MongoDb connect failed", err);
+    console.log("MongoDb connect failedd", err);
 })
 
 
