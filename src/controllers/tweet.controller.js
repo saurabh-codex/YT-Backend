11964c0d639a83350fb4ca4ae91486525b28a7d9
saurabh soni <130442880+saurabh-codex@users.noreply.github.com>
2024-06-10 19:21:55 +0530

    add tweet controllers

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,134 @@
+import mongoose, { isValidObjectId } from "mongoose"
+import {Tweet} from "../models/tweet.model.js"
+import {User} from "../models/user.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const createTweet = asyncHandler(async (req, res) => {
+    //TODO: create tweet
+    const{content} = req.body;
+
+    if(!content || content?.trim() === ""){
+        throw new ApiError(400, "content is required")
+    }
+    const tweet = Tweet.create({
+        content,
+        owner: req.user._id
+    })
+    if(!tweet){
+        throw new ApiError(500, "SWW while creating tweet")
+    }
+
+    return res.status(201)
+    .json(new ApiResponse(200,tweet,"tweet created successfully")
+    )
+})
+
+const getUserTweets = asyncHandler(async (req, res) => {
+    // TODO: get user tweets
+    const{userId} = req.params
+
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400, "thi user is not valid")
+    }
+
+    const user = await User.findById(userId)
+
+    if(!user){
+        throw new ApiError (404, " user not found")
+    }
+
+    const tweets = await Tweet.aggregate([
+        {
+            $match: {
+                owner: user._id
+            }
+        }
+    ])
+
+    if(!tweets){
+        throw new ApiError(500, "SWW while fetching tweets")
+    }
+
+    return res.status(201)
+    .json(new ApiResponse(200, tweets, "tweets fetched successfully"))
+})
+
+const updateTweet = asyncHandler(async (req, res) => {
+    //TODO: update tweet
+    const{newContent} = req.body
+    const{tweetId} = req.params
+
+    if(!newContent || newContent?.trim() === ""){
+        throw new ApiError(400, "content is required")
+    }
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "this tweet id is not valid")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if(!tweet){
+        throw new ApiError(404, " tweet not found")
+    }
+    
+    if(tweet.owner.toString() !== req.user._id.toString()){
+        throw new ApiError(403, "u cant update this post")
+    }
+
+    const updateTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set:{
+                content: newContent
+            }
+        },
+        {
+            new : true
+        }
+    )
+
+    if(!updateTweet){
+        throw new ApiError(500, "SWW while updatingtweet")
+    }
+
+    return res.status(201)
+    .json(new ApiResponse(200,updateTweet,"tweet updated successfully"))
+})
+
+const deleteTweet = asyncHandler(async (req, res) => {
+    //TODO: delete tweet
+    const {tweetId} = req.params
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400,"this tweet id is not valid")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if(!tweet){
+        throw new ApiError(404, "no tweet found")
+    }
+
+    if(tweet.owner.toString() !== req.user._id.toString()){
+        throw new ApiError(403,"u dont ahve permission to dlt this post")
+    }
+
+    const deleteTweet = await Tweet.deleteOne(req.user._id)
+
+    if(!deleteTweet){
+        throw new ApiError(500, "SWW while deleting tweet")
+    }
+
+    return res.status(201)
+    .json(new ApiResponse(200, deleteTweet, "tweet deleted successfully"))
+})
+
+export {
+    createTweet,
+    getUserTweets,
+    updateTweet,
+    deleteTweet
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,45 +1,60 @@
 import { Router } from "express";
-import { changeCurrentUserPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
+import {
+  changeCurrentUserPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name : "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-
-
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 // secured routes
 
-router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
 
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
 
-router.route("/change-password").post(verifyJWT,changeCurrentUserPassword)
+router.route("/change-password").post(verifyJWT, changeCurrentUserPassword);
 
-router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 
-router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
-router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
-router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
-router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 
-router.route("/history").get(verifyJWT,getWatchHistory)
+router.route("/history").get(verifyJWT, getWatchHistory);
 
-export default router
+export default router;
